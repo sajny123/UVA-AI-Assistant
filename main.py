af491c88932b93445981b2c33c17fdbeedb19c73
Sajid Islam <sajnyc360@gmail.com>
2025-10-12 23:01:28 -0400

    changed port which server was running on

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from app.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
