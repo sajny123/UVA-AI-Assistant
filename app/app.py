2c5b7ba6c46c7606450b535572216c4be2ab5c2d
Sajid Islam <sajnyc360@gmail.com>
2025-10-14 00:01:21 -0400

    chore: fixed connect of frontend and backend

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from app.routes import setup_routes
 from dotenv import load_dotenv
 
 def create_app():
     load_dotenv()
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend/project/dist', static_url_path='/')
     app.config["JSON_SORT_KEYS"] = False
     
     setup_routes(app)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        return send_from_directory(app.static_folder, 'index.html')
 
     return app
