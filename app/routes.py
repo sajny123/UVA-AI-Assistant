f6fd461174658057b004ab5b6b6163b8fc52753d
Sajid Islam <sajnyc360@gmail.com>
2025-10-17 00:46:05 -0400

    connected n8n workflow

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app.services.claude_client import MCP
 from flask import request, jsonify, render_template
+import httpx, os
 
 def setup_routes(app):
         
@@ -11,4 +12,13 @@ def setup_routes(app):
     def ask():
         user_input = request.json.get("question", "")
         answer = MCP.ask_claude(user_input)
+        
+        try:
+            n8n_url = os.environ.get('n8n_WEBHOOK_URL')
+            data = {"question": user_input}, {"answer": answer}
+            httpx.post(n8n_url, json=data)
+            print("n8n connected")
+        except Exception as e:
+            print(f"An error occured while posting to n8n: {e}")
+
         return jsonify({"answer": answer})
